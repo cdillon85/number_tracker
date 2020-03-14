8d43c08047eed8bc76e411309e9908294cf18504
Christine Dillon <christine.dillon@gmail.com>
2020-03-14 09:01:14 -0400

    set up urls and basic views

---
+++ tracker/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('numbers/', views.get_all_numbers, name="get_all_numbers"),
+    path('number/', views.track_or_increment_number, name="track_or_increment_number"),
+    path('number/<int:number>', views.delete, name="delete"),
+]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'tracker/index.html')
+
+def track_or_increment_number(request):
+    return HttpResponse("Add or update a number.")
+
+def get_all_numbers(request):
+    return HttpResponse("Here's all the numbers you have tracked so far.")
+
+def delete(request, number):
+    return HttpResponse(f"You deleted the number {number}")
